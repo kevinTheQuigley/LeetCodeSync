3d35058a25a2495d6f58c72c2189dcad4f8274ef
Kevin Quigley <65398673+kevinTheQuigley@users.noreply.github.com>
2026-05-13 01:41:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (41.78%)

--- my-folder/0002-add-two-numbers/solution.py
+++ my-folder/0002-add-two-numbers/solution.py
@@ -1,37 +1,28 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+        remainder = 0
+        dummyNode = ListNode()
+        startNode = ListNode(next=dummyNode)
+
+        while l1 or l2 or remainder:
+            dummyNode.next = ListNode()
+            dummyNode= dummyNode.next
+            total = 0 
+            if l1:
+                total+=l1.val
+                l1=l1.next
+
+            if l2:
+                total+=l2.val
+                l2=l2.next
+
+            total+=remainder
+
+            remainder = total//10
+            total = total%10
+
+            dummyNode.val=total
         
-        carry = 0
+        return startNode.next.next
 
-        head = ListNode()
-        current = head
-        
-        while (l1 != None )or (l2 != None or carry !=0):
-            l1_val = l1.val if l1!=None else 0
-            l2_val = l2.val if l2!=None else 0
-            temp = carry+l1_val+l2_val  
-
-            carry = temp//10
-            temp = temp %10
-
-            current.next = ListNode(temp)
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-            current = current.next
-
-        return head.next
-
-
-
-
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        
