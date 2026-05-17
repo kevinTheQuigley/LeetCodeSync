d58cf197a4f3e1710302938c3719f0f5ad398465
Kevin Quigley <65398673+kevinTheQuigley@users.noreply.github.com>
2026-05-17 04:44:27 +0000

    [LeetCode Sync] Runtime - 75 ms (5.09%), Memory - 29.7 MB (14.46%)

--- my-folder/0011-container-with-most-water/solution.py
+++ my-folder/0011-container-with-most-water/solution.py
@@ -1,37 +1,21 @@
 
-
-'''
-To calculate the volume, 
-multiply the minimum height of the two pointers by the distance between them.
-(i-j)*(min(nums[i],nums[j]))
-
-Makes sense to start with the two pointers as far away as possible and iterate inwards. 
-Think about a situation where you have two giant poles right in the middle.
-You only need to store the max, so you don't need to use dynamic programming.
-I guess you stay at the closest maximum and work inwards
-
-
-
-
-
-'''
-
-class Solution(object):
-    def maxArea(self, height):
-        i,j = 0,len(height)-1
-        maxVolume = 0 
-        while i!=j:
-            volume = (j-i)*(min(height[i],height[j]))
-            if volume >maxVolume:
-                maxVolume = volume
-            if height[i]>height[j]:
-                j =j-1
-            else:
-                i = i+1
-        return maxVolume
-
-        """
-        :type height: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
         
+
+
+        def computeArea(left: int,right: int,height)->int:
+            h= min(height[left],height[right])
+            area = (right-left)*h
+            return area
+
+        l = maxArea = 0
+        r=len(height)-1        
+        while l<r:
+            area = computeArea(l,r,height)
+            if height[l]<height[r]:
+                l+=1
+            else:
+                r-=1
+            maxArea=max(area,maxArea)
+        return maxArea
