abbd3540581271aad55db18dee4083bc2dfe6856
Kevin Quigley <65398673+kevinTheQuigley@users.noreply.github.com>
2026-05-26 00:55:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (9.99%)

--- my-folder/0017-letter-combinations-of-a-phone-number/solution.py
+++ my-folder/0017-letter-combinations-of-a-phone-number/solution.py
@@ -1,20 +1,32 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        digitDict ={"2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
-        returnList = []
-        def backTrack(digits,string):
-            if len(digits)==0:
-                return None
-            digit = digits[0]
-            letters = digitDict[digit]
-            if len(digits[1:])==0:
-                for letter in letters:
-                    returnList.append(string+letter)
+        letDict={
+            '2':'abc',
+            '3':'def',
+            '4':'ghi',
+            '5':'jkl',
+            '6':'mno',
+            '7':'pqrs',
+            '8':'tuv',
+            '9':'wxyz'
+        }
+        charList=None
+
+        while len(digits)>0:
+
+            d=digits[0]
+            digits=digits[1:]
+
+            if charList is None:
+                nextCharList=[char for char in letDict[d]]
             else:
-                for letter in letters:
-                    backTrack(digits[1:],string+letter)
-        backTrack(digits,"")
-        return returnList
+                nextCharList=[]
+                for char in charList:
+                    for substring in letDict[d]:
+                        nextCharList.append(char+substring)
+            charList=nextCharList
+        if charList is None:
+            return []
+        else:
+            return charList
 
-
-        
