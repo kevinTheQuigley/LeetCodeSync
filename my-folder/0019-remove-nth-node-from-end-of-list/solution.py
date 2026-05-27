bafe6d44fcb6c5160869ea2572403a52a8fc95fd
Kevin Quigley <65398673+kevinTheQuigley@users.noreply.github.com>
2026-05-27 01:44:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.2 MB (91.11%)

--- my-folder/0019-remove-nth-node-from-end-of-list/solution.py
+++ my-folder/0019-remove-nth-node-from-end-of-list/solution.py
@@ -1,39 +1,22 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution(object):
-    def removeNthFromEnd(self, head, n):
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
 
-        if head.next is None or head is None:
-            return None
-        
-        firstPointer = head
-        secondPointer = head
-        iterator =0
-        while firstPointer.next is not None or iterator <n :
-            if firstPointer.next is None and iterator <n :
-                firstPointer = head
-            else:
-                firstPointer = firstPointer.next
-            if iterator >=n:
-                secondPointer = secondPointer.next
-            iterator +=1
-        if secondPointer.next is not None:
-            secondPointer.next = secondPointer.next.next
-        else:
-            head = head.next
+        firstPointer=head
+        dummy = ListNode(next=head)
 
-        return head
+        while n>0:
+            firstPointer=firstPointer.next
+            n-=1
+        secondPointer=dummy
+        if firstPointer is None:
+            print('fp is none')
+        while firstPointer is not None:
+            firstPointer=firstPointer.next
+            secondPointer=secondPointer.next
             
-
-
-
-        return head
-        """
-        :type head: Optional[ListNode]
-        :type n: int
-        :rtype: Optional[ListNode]
-        """
+        if secondPointer.next is head:
+            return head.next 
+        else:
+            secondPointer.next = secondPointer.next.next
+            return head
         
