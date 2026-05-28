f7b7ffae3f8ceb58cdc1c4bd44b77a6c8ac8a01c
Kevin Quigley <65398673+kevinTheQuigley@users.noreply.github.com>
2026-05-28 01:50:14 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (34.92%)

--- my-folder/0022-generate-parentheses/solution.py
+++ my-folder/0022-generate-parentheses/solution.py
@@ -1,30 +1,21 @@
-'''
-So here we can use a backtracking recursive solution.
-We need to keep track of two stacks, the left braces and the right braces.
-We can't use a right brace before using a left brace. How should we keep track of this? I think we should only add to the list of right brackets after addingto the left.
-So you pop one off the leftStack. Then you can call the function again
-
-
-'''
-
-
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        leftStack = n*"("
-        rightStack = ""
-        returnList = []
 
-        def dfs(leftS:str,rightS:str,currentS):
+        returnList= []
 
-            if len(leftS)==0 and len(rightS)==0:
-                returnList.append(currentS)
-            else:
-                if len(leftS) >0:
-                    dfs(leftS[1:],rightS+")",currentS+leftS[0])
-                if len(rightS)>0:
-                    dfs(leftS,rightS[1:],currentS+rightS[0])
+        def generateParentheses(positivity,parenthesesLeft,currentString):
+            
+            if positivity >0:
+                # Positivity indicates the number of open parentheses already used
+                # Parenthesesleft indicates the number of open parethenses not used
+                generateParentheses(positivity-1,parenthesesLeft,currentString+")")
+            if parenthesesLeft>0:
+                generateParentheses(positivity+1,parenthesesLeft-1,currentString+"(")
+            
+            if positivity==0 and parenthesesLeft==0:
+                returnList.append(currentString)
         
-        dfs(leftStack,rightStack,"")
+        generateParentheses(0,n,"")
+
         return returnList
 
-        
