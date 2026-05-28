f136d7348ec769d394c32e7de5f258f9a2e56d20
Kevin Quigley <65398673+kevinTheQuigley@users.noreply.github.com>
2026-05-28 01:37:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (33.04%)

--- my-folder/0021-merge-two-sorted-lists/solution.py
+++ my-folder/0021-merge-two-sorted-lists/solution.py
@@ -1,36 +1,24 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        returnNode = ListNode()
-        firstNode = returnNode
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
 
-        while list1 is not None or list2 is not None:
-            if list1 is not None and list2 is None:
-                returnNode.next = ListNode(list1.val)
-                returnNode = returnNode.next
-                list1 = list1.next
-            elif list1 is None and list2 is not None:
-                returnNode.next = ListNode(list2.val)
-                returnNode = returnNode.next
-                list2 = list2.next
+        dummyNode = ListNode()
+        headNode = dummyNode
+
+        while  not (list1 is None or list2 is None):
+
+            if list1.val >list2.val:
+                dummyNode.next = list2
+                list2=list2.next
+                dummyNode=dummyNode.next
             else:
-                if list1.val>list2.val:
-                    returnNode.next = ListNode(list2.val)
-                    returnNode = returnNode.next
-                    list2 = list2.next
-                else:
-                    returnNode.next = ListNode(list1.val)
-                    returnNode = returnNode.next
-                    list1 = list1.next
-        return firstNode.next
+                dummyNode.next=list1
+                list1=list1.next
+                dummyNode=dummyNode.next
+
+        else:
+            if list1 is None:
+                dummyNode.next=list2
+            else:
+                dummyNode.next=list1
+        return headNode.next
             
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        
