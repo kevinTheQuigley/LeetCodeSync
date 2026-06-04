d6b8501e94c2cd31eee1763cdd79b376df29ee21
Kevin Quigley <65398673+kevinTheQuigley@users.noreply.github.com>
2026-06-04 01:23:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (40.22%)

--- my-folder/0017-letter-combinations-of-a-phone-number/solution.py
+++ my-folder/0017-letter-combinations-of-a-phone-number/solution.py
@@ -1,32 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+
         letDict={
-            '2':'abc',
-            '3':'def',
-            '4':'ghi',
-            '5':'jkl',
-            '6':'mno',
-            '7':'pqrs',
-            '8':'tuv',
-            '9':'wxyz'
+            "2":"abc",
+            "3":"def",
+            "4":"ghi",
+            "5":"jkl",
+            "6":"mno",
+            "7":"pqrs",
+            "8":"tuv",
+            "9":"wxyz"
         }
-        charList=None
 
-        while len(digits)>0:
-
-            d=digits[0]
-            digits=digits[1:]
-
-            if charList is None:
-                nextCharList=[char for char in letDict[d]]
+        def generateLetters(inputStr,remainingDigits):
+            if remainingDigits =="":
+                return [inputStr]
             else:
-                nextCharList=[]
-                for char in charList:
-                    for substring in letDict[d]:
-                        nextCharList.append(char+substring)
-            charList=nextCharList
-        if charList is None:
-            return []
-        else:
-            return charList
+                returnList = []
+                letters = letDict[remainingDigits[0]]
+                for letter in letters:
+                    returnList.extend(generateLetters(inputStr+letter,remainingDigits[1:]))
+                return returnList
+            
+        return generateLetters('',digits)
 
